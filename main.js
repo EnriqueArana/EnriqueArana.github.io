61b8de9b639cee08c260b4ef1ea52753a185b04c
Enrique Arana <jaranar15_2@unc.edu.pe>
2023-04-10 12:57:06 -0500

    Vista responsive desktop y tablet | Home

--- main.js
+++ main.js
@@ -1,14 +1,10 @@
 const nav = document.querySelector('nav');
-const skills= document.querySelector('.skills');
 
 window.addEventListener('scroll',()=>{
     // nav.classList.toggle('active',window.scrollY >0);
     if (window.scrollY > 0) {
         nav.classList.add('active');
-        skills.classList.add('skills-show');
       } else {
         nav.classList.remove('active');
-        skills.classList.remove('skills-show');
       }
 })
-
